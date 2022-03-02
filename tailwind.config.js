352cd17836358c9f407606eedc36868ad6cf8401
samuel <samuel.potter@epitech.eu>
2022-03-02 20:12:18 +0400

    readme and preview gif added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Gotham: ['Gotham']
+      }
+    },
   },
   plugins: [],
 }
